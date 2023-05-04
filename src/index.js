4f9924cf7a82a574f2c05dfeadd1929b1634bd90
Exiels <Samina29@outlook.fr>
2023-05-04 20:57:11 +0200

    [ADD] Added informations and updated .env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config({ path: '../.env'})
 
 const app = express()
 const port = process.env.EXPRESS_PORT
