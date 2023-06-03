9e8568fee9bf1214afd71d30347eb38d955078cf
Exiels <Samina29@outlook.fr>
2023-06-03 13:17:06 +0200

    [ADD] Added profile

---
+++ src/routes/user/profile.js
@@ -0,0 +1,47 @@
+/**
+ * @memberof module:router~mainRouter~userRouter
+ * @inner
+ * @namespace profile
+ */
+const { Classes } = require('../../models/classes')
+const { Roles } = require('../../models/roles')
+const { Users } = require('../../models/users')
+
+/**
+ * Main profile function
+ * @name GET /user/profile
+ * @function
+ * @memberof module:router~mainRouter~userRouter~profile
+ * @inner
+ * @async
+ * @param {Object} req
+ * @param {Object} res
+ * @returns 200 if OK and return access token and role name
+ * @returns 500 if Internal Server Error
+ */
+module.exports = async (req, res) => {
+  try {
+    const user = await Users.findById(req.user._id)
+    const userRole = await Roles.findById(user.role)
+    const classes = []
+
+    for (const _class of user.classes) {
+        console.log(_class)
+        const className = await Classes.findById(_class)
+        classes.push(className.name)
+    }
+
+    const response = {
+        firstname: user.firstname,
+        lastname: user.lastname,
+        email: user.email,
+        role: userRole.name,
+        classes
+    }
+    // Send profile
+    return res.status(200).json(response)
+  } catch (error) /* istanbul ignore next */ {
+    console.error(error)
+    return res.status(500).json({ message: 'Internal Server Error' })
+  }
+}
--- src/routes/user/router.js
+++ src/routes/user/router.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const auth = require('../../middleware/auth')
+const access = require('../../middleware/access')
+
 const login = require('./login')
 const forgottenPassword = require('./forgottenPassword')
+const profile = require('./profile')
 
 /**
  * User router connection
@@ -14,5 +18,6 @@ const forgottenPassword = require('./forgottenPassword')
 // Created router routes connection
 router.post('/login', login)
 router.post('/forgottenPassword', forgottenPassword)
+router.get('/profile', auth, access(0), profile)
 
 module.exports = router
