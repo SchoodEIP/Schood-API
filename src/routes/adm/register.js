487d94ee7310ceed2c4d2a3f0c68c027b68f3d41
adbusnel <adrien.busnel@epitech.eu>
2023-08-26 20:37:23 +0200

    [MOD] fixed recuperation of role, iteration through classes

--- src/routes/adm/register.js
+++ src/routes/adm/register.js
@@ -28,16 +28,26 @@ const random = require('random-string-generator')
 module.exports = async (req, res) => {
   try {
     /* istanbul ignore next */
-    const mail = Boolean((req.query.mail || '').replace(/\s*(false|null|undefined|0)\s*/i, ''))
+    const mail = Boolean((req.query.email || '').replace(/\s*(false|null|undefined|0)\s*/i, ''))
+    const role = await Roles.findOne({ name: req.body.role });
+
+    const data = {
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      role: role._id.toString(),
+      classes: req.body.classes
+    };
 
     // Verif received data
-    const { error } = validateRegister(req.body)
+    const { error } = validateRegister(data)
+
     if (error) {
       return res.status(400).json({ message: 'Invalid request' })
     }
 
     // Check if role exist
-    const role = await Roles.findById(req.body.role)
+    // const role = await Roles.findById(req.body.role)
     if (!role || role === undefined || role.length === 0) {
       return res.status(400).json({ message: 'Invalid role' })
     }
@@ -47,11 +57,10 @@ module.exports = async (req, res) => {
     if (role.name === 'student' && classesRequest.length > 1) {
       return res.status(400).json({ message: 'Student can only have 1 class' })
     }
-
     // Check classes
     const classes = []
-    for (const element of classesRequest) {
-      const class_ = await Classes.findById(element)
+    for (let i = 0; i < classesRequest.length; i++) {
+      const class_ = await Classes.findOne({name: classesRequest[i]})
 
       if (!class_ || class_ === undefined || class_.length === 0) {
         return res.status(400).json({ message: 'Invalid class' })
@@ -82,7 +91,6 @@ module.exports = async (req, res) => {
           sendMail(req.body.email, 'Schood Account Created', message)
         }
       })
-
     return res.status(200).send()
   } catch (error) /* istanbul ignore next */ {
     console.error(error)
