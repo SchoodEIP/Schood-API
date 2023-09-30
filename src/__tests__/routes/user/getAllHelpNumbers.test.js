21b2bae533696413d5ed0a47122083311a8cd2ae
Quentin Mananes <quentin.mananes@epitech.eu>
2023-09-30 14:45:55 +0200

    [Add] Create route for getting all the helpNumbers

---
+++ src/__tests__/routes/user/getAllHelpNumbers.test.js
@@ -0,0 +1,54 @@
+const request = require('supertest')
+const mongoose = require('mongoose')
+
+const server = require('../../serverUtils/testServer')
+const dbDefault = require('../../../config/db.default')
+
+describe('User route tests', () => {
+  let app
+
+  beforeAll(async () => {
+    process.env.PROD = true
+    app = await server.testServer()
+  })
+
+  afterEach(async () => {
+    const collections = mongoose.connection.collections
+    for (const key in collections) {
+      const collection = collections[key]
+      await collection.deleteMany()
+    }
+    await dbDefault()
+  })
+
+  afterAll(async () => {
+    await mongoose.connection.dropDatabase()
+    await mongoose.connection.close()
+  })
+
+  describe('getAllHelpNumbers route', () => {
+    it('GET /user/helpNumbers => Try get all help numbers', async () => {
+      let key
+
+      await request(app)
+        .post('/user/login')
+        .send({
+          email: 'adm@schood.fr',
+          password: 'adm123'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          key = response.body.token
+        })
+
+      return await request(app)
+        .get('/user/helpNumbers')
+        .set({
+          'x-auth-token': key
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+    })
+  })
+})
