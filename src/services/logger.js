999f01444fcf38f132bb08f4d7f96e78f9ea16a5
Quentin Mananes <quentin.mananes@epitech.eu>
2024-02-02 19:00:34 +0100

    [Add] Delete helpNumbersCategory

--- src/services/logger.js
+++ src/services/logger.js
@@ -1,5 +1,5 @@
 class Logger {
-  static displayed = process.env.LOGGER || true
+  static displayed = process.env.LOGGER
 
   static info (str) {
     if (this.displayed) console.log((new Date().toLocaleTimeString()), '\x1b[36m', str, '\x1b[39m')
