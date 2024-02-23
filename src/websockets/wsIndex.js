5b1197eff66e74844a6d33e4e4cb7204292c07f1
Quentin Mananes <quentin.mananes@epitech.eu>
2024-02-23 20:06:16 +0100

    [Add] Websockets, chat features CreateChat and messageChat

--- src/websockets/wsIndex.js
+++ src/websockets/wsIndex.js
@@ -1,13 +1,60 @@
+const parser = require('./parser')
+
 module.exports = (wss) => {
+  const clients = {}
+
   wss.on('connection', (ws) => {
+    ws.isAlive = true
     ws.on('error', console.error)
 
     ws.on('message', (data) => {
-      console.log('received: %s', data)
+      const parsedData = JSON.parse(data)
+
+      if (parsedData.method === 'login') {
+        addClient(parsedData.data.userId)
+      } else if (parsedData.data.method === 'close') {
+        removeClient(ws)
+      } else {
+        parser(parsedData, clients)
+      }
     })
 
     ws.send(JSON.stringify({
-      message: 'connected'
+      data: {
+        message: 'connected'
+      }
     }))
+
+    ws.on('close', () => {
+      removeClient()
+    })
+
+    ws.on('pong', () => {
+      ws.isAlive = true
+    })
+
+    const addClient = (userId) => {
+      clients[userId] = ws
+    }
+
+    const removeClient = () => {
+      for (const client in clients) {
+        if (clients[client] === ws) {
+          delete clients[client]
+        }
+      }
+    }
   })
+
+  setInterval(() => {
+    Object.keys(clients).forEach((client) => {
+      if (clients[client].isAlive === false) {
+        clients[client].terminate()
+        delete clients[client]
+      } else {
+        clients[client].isAlive = false
+        clients[client].ping()
+      }
+    })
+  }, 5000)
 }
