799f8e6e0a7f625a50cb7345d3a18e698c0ca52e
Quentin Mananes <quentin.mananes@epitech.eu>
2024-02-27 11:29:09 +0100

    [Fix] logger Recreate the console.log function behavior with an infinite number of parameters

--- src/config/db.default.js
+++ src/config/db.default.js
@@ -48,7 +48,6 @@ module.exports = async (test = false) => {
     }
     if (test) Logger.displayed = true
   } catch (error) {
-    console.error(error)
     Logger.error('ERROR: Init default data : ', error)
   }
 }
