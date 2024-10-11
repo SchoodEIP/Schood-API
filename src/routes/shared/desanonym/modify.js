c960ac3ebba2db8f30a7fdef0b0c263fe2769e0d
Exiels <Samina29@outlook.fr>
2024-10-11 14:42:48 +0200

    [ADD] Added desanonym

---
+++ src/routes/shared/desanonym/modify.js
@@ -0,0 +1,63 @@
+/**
+ * @memberof module:router~mainRouter~desanonymsRouter
+ * @inner
+ * @namespace desanonyms
+ */
+
+const { default: mongoose } = require('mongoose')
+const { Desanonyms, validateModify } = require('../../../models/desanonym')
+
+/**
+ * Main desanonyms function
+ * @name PATCH /shared/desanonyms
+ * @function
+ * @memberof module:router~mainRouter~desanonymsRouter~desanonyms
+ * @inner
+ * @async
+ * @param {Object} req
+ * @param {Object} res
+ * @returns 200 if OK and return access token and role name
+ * @returns 400 if Invalid arguments
+ * @returns 500 if Internal Server Error
+ */
+module.exports = async (req, res) => {
+  try {
+    // Verif received data
+    const id = req.params.id
+    const { error } = validateModify(req.body)
+    if (error) {
+      return res.status(400).json({ message: 'Invalid request' })
+    }
+
+    if (req.user.role.levelOfAccess === 0) {
+      if (req.body.reason || res.body.message) {
+        return res.status(400).json({ message: 'Invalid request' })
+      }
+    }
+
+    if (req.user.role.levelOfAccess === 2) {
+      if (req.body.status) {
+        return res.status(400).json({ message: 'Invalid request' })
+      }
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: 'Invalid request' })
+    }
+    const desanonym = await Desanonyms.findById(id)
+    if (!desanonym) {
+      return res.status(400).json({ message: 'Invalid request' })
+    }
+
+    desanonym.reason = req.body.reason ? req.body.reason : desanonym.reason
+    desanonym.message = req.body.message ? req.body.message : desanonym.message
+    desanonym.status = req.body.status ? req.body.status : desanonym.status
+
+    await desanonym.save()
+
+    return res.status(200).send()
+  } catch (error) /* istanbul ignore next */ {
+    console.error(error)
+    return res.status(500).json({ message: 'Internal Server Error' })
+  }
+}
